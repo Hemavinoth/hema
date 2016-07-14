8d28e5ea441c2fce92c24bd8d905063486305b12
Hemavinoth <avhemavinoth@gmail.com>
2016-07-14 15:01:42 +0530

    Create README.c

---
+++ README.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+double number;
+printf("enter the number");
+scanf("%d",&num);
+if(num<=0.0)
+{
+printf("enter the zero");
+else
+printf("negative");
+}
+else
+{
+printf("positive");
+}
+}
